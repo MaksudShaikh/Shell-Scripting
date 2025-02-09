846b15d3f501e3ccedf9d6e448a0bee432078462
MaksudShaikh <142396108+MaksudShaikh@users.noreply.github.com>
2025-02-10 01:34:11 +0530

    q9.sh

--- q9.sh
+++ q9.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Write a shell script to print the following pattern
-# *
-# **
-# ***
-# ****
-
 i=1
 while [ $i -le 4 ]; do
     j=1
-    while [ $j -le $i  ]; do
-        echo "* \c"
-        let j+=1
+    while [ $j -le $i ]; do
+        echo -n "* "   # Use -n to prevent a newline
+        j=$((j+1))      # Increment j
     done
-    echo
-    let i+=1
+    echo              # Print newline
+    i=$((i+1))        # Increment i
 done
